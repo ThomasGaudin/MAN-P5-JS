17b032a2ef03d333511987e5b9554374c19ba952
ThomasGaudin <95482027+ThomasGaudin@users.noreply.github.com>
2025-09-08 15:35:09 +0200

    grid push

---
+++ 00_boilerplates/1_p5-basics-line/sketch.js
@@ -0,0 +1,35 @@
+const grid = {
+  numColumns: 5,
+};
+
+const ball = {
+  diameter: 20,
+  color: "black",
+};
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  pixelDensity(2);
+  background("white");
+  noStroke();
+  fill("black");
+
+  for (
+    let nombreEllipse = 0;
+    nombreEllipse < grid.numColumns;
+    nombreEllipse = nombreEllipse + 1
+  ) {
+    const sizeColumn = windowWidth / grid.numColumns;
+
+    const positionBall = {
+      x: sizeColumn * nombreEllipse + sizeColumn / 2,
+      y: windowHeight / 2,
+    };
+    ellipse(positionBall.x, positionBall.y, ball.diameter);
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  setup();
+}
--- 00_boilerplates/p5-empty/sketch.js
+++ 00_boilerplates/p5-empty/sketch.js
@@ -1,17 +1,40 @@
-// global functions here
+let grid = {
+  numColumns: 10,
+  numRows: 10,
+};
+
+let ball = {
+  diameter: 10,
+  color: "black",
+};
 
 function setup() {
-  createCanvas(0, 0)
-  windowResized()
-  // 
+  createCanvas(windowWidth, windowHeight);
+  pixelDensity(2);
+  windowResized();
 }
 
 function draw() {
-  background('black')
-  
-  // your code
+  background("white");
+  let valueRadius = map(mouseX, 0, windowWidth, 1, 75);
+  fill("black");
+  noStroke();
+  let columnWidth = windowWidth / grid.numColumns;
+  let rowWidth = windowHeight / grid.numRows;
+  for (let j = 0; j < grid.numRows; j++) {
+    for (let i = 0; i < grid.numColumns; i++) {
+      ellipse(
+        columnWidth * i + columnWidth / 2,
+        rowWidth * j + rowWidth / 2,
+        ball.diameter,
+        ball.diameter
+      );
+    }
+  }
+
+  ball.diameter = valueRadius;
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight)
+  resizeCanvas(windowWidth, windowHeight);
 }
